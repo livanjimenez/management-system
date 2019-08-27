780398bf238080de20a2a67079f268225031e29f
Livan Jimenez <jimenezlivan14@gmail.com>
2019-08-27 17:43:16 -0400

    dash

--- client/src/App.js
+++ client/src/App.js
@@ -10,3 +10,13 @@ function App() {
 }
 
 export default App;
+
+
+/**
+ * Colors:
+ * #1A1A1D ; black?
+ * #4E4E50 ; gray?
+ * #950740 ; dark bright red?
+ * #6F2232 ; brick red?
+ * #C3073F ; bright brick red?
+ */
