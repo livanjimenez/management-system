664a0d26241851cb393afb075907d7506cfc0d97
Livan Jimenez <jimenezlivan14@gmail.com>
2019-08-27 16:08:52 -0400

    concurrently setup

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const app = express();
 
 const port = process.env.PORT || 8000;
 
+app.get('/', (req, res) => {
+  res.send('HELLO WORLD');
+});
+
 app.listen(port, () => console.log(`Listening on ${port}`));
