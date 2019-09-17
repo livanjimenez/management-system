a115dfc36da30139a6cbb9b02de8b83db3aafac0
Livan Jimenez <jimenez.livan14@gmail.com>
2019-09-17 11:21:25 -0400

    icon fix

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import { ChevronLeft, ChevronRight } from "@material-ui/icons";
 import {
-<<<<<<< HEAD
-=======
   Divider,
   List,
   ListItem,
   ListItemText,
   ListItemIcon,
->>>>>>> b1dcc19dbee3b0b12fdadf4169f401e5d24287cd
   Icon,
 } from '@material-ui/core';
 import { Nav } from 'mui-layout';
 import FitnessCenterIcon from '@material-ui/icons/FitnessCenter';
 
-<<<<<<< HEAD
-=======
 const list = [
   {
     primaryText: "My Files",
@@ -51,42 +46,37 @@ const list = [
   }
 ];
 
->>>>>>> b1dcc19dbee3b0b12fdadf4169f401e5d24287cd
 const Navbar = () => {
-  return(
+  return (
     <div>
       <Nav renderIcon={collapsed => collapsed ? <ChevronRight /> : <ChevronLeft />}>
-<<<<<<< HEAD
-        <Icon>add_circle</Icon>
-=======
         <h3>General Title</h3>
         <Divider />
         <List>
-    {list.map(({ primaryText, icon }, i) => (
-      <ListItem key={primaryText} selected={i === 0} button>
-        <ListItemIcon>
-          <Icon>{icon}</Icon>
-        </ListItemIcon>
-        <ListItemText
-          primary={primaryText}
-          primaryTypographyProps={{ noWrap: true }}
-        />
-      </ListItem>
-    ))}
-    <Divider style={{ margin: "12px 0" }} />
-    <ListItem button>
-      <ListItemIcon>
-        <Icon>
-          <FitnessCenterIcon />
-        </Icon>
-      </ListItemIcon>
-      <ListItemText
-        primary={"Settings & account"}
-        primaryTypographyProps={{ noWrap: true }}
-      />
-    </ListItem>
-  </List>
->>>>>>> b1dcc19dbee3b0b12fdadf4169f401e5d24287cd
+          {list.map(({ primaryText, icon }, i) => (
+            <ListItem key={primaryText} selected={i === 0} button>
+              <ListItemIcon>
+                <Icon>{icon}</Icon>
+              </ListItemIcon>
+              <ListItemText
+                primary={primaryText}
+                primaryTypographyProps={{ noWrap: true }}
+              />
+            </ListItem>
+          ))}
+          <Divider style={{ margin: "12px 0" }} />
+          <ListItem button>
+            <ListItemIcon>
+              <Icon>
+                <FitnessCenterIcon />
+              </Icon>
+            </ListItemIcon>
+            <ListItemText
+              primary={"Settings & account"}
+              primaryTypographyProps={{ noWrap: true }}
+            />
+          </ListItem>
+        </List>
       </Nav>
     </div>
   );
