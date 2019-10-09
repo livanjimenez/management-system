f0ccf62d5bd6d6e74d423bd724ae0107bd1c4ee1
Livan Jimenez <jimenez.livan14@gmail.com>
2019-10-08 22:39:23 -0400

    setting up restful api

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-
-const port = process.env.PORT || 8000;
-
-app.get('/', (req, res) => {
-  res.send('HELLO WORLD');
-});
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => console.log(`Listening on ${port}`));
+// parse JSON (application/json content-type)
+app.use(bodyParser.json());
+
+let data = require('./data');
+
+app.get('/items', (req, res) => {
+  res.json(data);
+});
+
+app.get('/items/:id', (req, res) => {
+  const itemId = req.params.id;
+  const item = data.find(_item => _item.id === itemId);
+  
+  if (item) {
+    res.json(item);
+  } else {
+    res.json({message: `item ${itemId} doesn't exist!` });
+  }
+});
+
+app.post('/items', (req, res) => {
+  const item = req.body;
+  console.log('Adding new item: ', item);
+
+  data.push(item);
+  res.json(data);
+});
