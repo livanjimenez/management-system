d470cafbbb26dcf2a2133ba3d78a0248ad771b0e
Livan Jimenez <jimenez.livan14@gmail.com>
2019-10-08 22:49:21 -0400

    remove nonsense

--- index.js
+++ index.js
@@ -7,27 +7,6 @@ app.listen(port, () => console.log(`Listening on ${port}`));
 // parse JSON (application/json content-type)
 app.use(bodyParser.json());
 
-let data = require('./data');
-
-app.get('/items', (req, res) => {
-  res.json(data);
-});
-
-app.get('/items/:id', (req, res) => {
-  const itemId = req.params.id;
-  const item = data.find(_item => _item.id === itemId);
-  
-  if (item) {
-    res.json(item);
-  } else {
-    res.json({message: `item ${itemId} doesn't exist!` });
-  }
-});
-
-app.post('/items', (req, res) => {
-  const item = req.body;
-  console.log('Adding new item: ', item);
-
-  data.push(item);
-  res.json(data);
+app.get('/', (req, res) => {
+  res.send('homepage for backend!');
 });
