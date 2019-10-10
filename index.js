0cd7e0749cb8af54f7adfd60c94620da8e8f9f6e
Livan Jimenez <jimenez.livan14@gmail.com>
2019-10-09 23:01:48 -0400

    hello backend

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || 8080;
+const routes = require('./routes/api');
 
+const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on ${port}`));
+
 // parse JSON (application/json content-type)
 app.use(bodyParser.json());
-
-app.get('/', (req, res) => {
-  res.send('homepage for backend!');
-});
+app.use('/api', routes);
---
+++ routes/api.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/modules', (req, res) =>{
+  res.send({type: 'GET'});
+});
+
+router.post('/modules', (req, res) =>{
+  res.send({type: 'POST'});
+});
+
+router.put('/modules/:id', (req, res) =>{
+  res.send({type: 'PUT'});
+});
+
+router.delete('/modules/:id', (req, res) =>{
+  res.send({type: 'DELETE'});
+});
