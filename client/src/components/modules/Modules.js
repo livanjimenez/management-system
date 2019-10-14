25ea0e4efa355098175af0b832c8efe13e21808e
Livan Jimenez <jimenez.livan14@gmail.com>
2019-10-14 15:09:11 -0400

    modules

--- client/src/components/modules/Modules.js
+++ client/src/components/modules/Modules.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(3),
+    textAlign: 'center',
+    height: 250,
+    width: 250,
+  },
+}));
 
 export default function Modules() {
+  const classes = useStyles();
+
   return(
-    <div>
-      
+    <div className={classes.root}>
+      <Grid item xs>
+        <Paper className={classes.paper}>
+          some text
+        </Paper>
+      </Grid>
+      <Grid item xs>
+        <Paper className={classes.paper}>
+          some text
+        </Paper>
+      </Grid>
     </div>
   );
 }
