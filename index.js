8e2615f98f0bb9d413b322a5c5add2604b588239
Livan Jimenez <jimenez.livan14@gmail.com>
2019-10-15 00:45:07 -0400

    finish schemas for modules

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-
-const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`Listening on ${port}`));
+const mongoose = require('mongoose');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,3 +10,16 @@ app.post('/user', (req ,res) =>{
   console.log(req.body);
   res.send(req.body);
 });
+
+// TODO: get mongodb atlas working with clusters and URI
+// MONGO_URI = ""
+
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose
+  .connect(MONGO_URI, {useNewUrlParser: true })
+  .then(() => console.log('Mongo connection is successful'))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening on ${port}`));
