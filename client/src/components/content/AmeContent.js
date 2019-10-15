b8d6577ea6e60100b70f36fcd3844dab7d914576
reinier042096 <43587989+reinier042096@users.noreply.github.com>
2019-10-14 23:06:31 -0400

    module update. module blank layout with expand and more option

--- client/src/components/content/AmeContent.js
+++ client/src/components/content/AmeContent.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Content } from 'mui-layout';
 import Form from '../forms/Form';
+import Modules from '../modules/Modules';
 
 const AmeContent = () => {
   return (
     <div>
       <Content>
+        <Modules/>
         <Form />
       </Content>
     </div>
