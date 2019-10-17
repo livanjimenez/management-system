304eb4fef1e292b2a823b0b3b2ee80daf8f3b92c
Livan Jimenez <jimenez.livan14@gmail.com>
2019-10-17 00:53:31 -0400

    user validation

---
+++ routes/api/user.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const SECRET = process.env.SECRET;
+const validateSignUpInput = require('../../validation/signup');
+const validateLoginInput = require('../../validation/login');
+const User = require('../../models/User');
+
