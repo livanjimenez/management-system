c8fedf5ca062f4510df911cd8b0ee040fa8f446f
Livan Jimenez <jimenez.livan14@gmail.com>
2019-10-18 18:44:42 -0400

    indexjs update for server and production

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const bodyParser = require('body-parser');
 const app = express();
 const mongoose = require('mongoose');
 const passport = require('passport');
+const path = require('path');
 
+// middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -13,7 +15,7 @@ app.use(passport.initialize());
 require('./middleware/passport')(passport);
 
 // Routes
-app.use('/api/users/', require('./routes/api/user'));
+app.use('/api/users/', require('./routes/api/users'));
 app.use('/api/posts/', require('./routes/api/posts'));
 
 app.post('/user', (req, res) => {
@@ -27,6 +29,18 @@ mongoose
   .connect(MONGO_URI, { useNewUrlParser: true })
   .then(() => console.log('Mongo connection is successful'))
   .catch(err => console.log(err));
+
+mongoose.set('useFindAndModify', false);
+mongoose.Promise = global.Promise;
+
+// Ready for production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on ${port}`));
