8d423aa5f2105695b84081a15ec396b40f60ff55
Livan Jimenez <jimenezlivan14@gmail.com>
2019-11-04 19:12:08 -0500

    cleaning

--- routes/module.routes.js
+++ routes/module.routes.js
@@ -1,13 +1,8 @@
 module.exports = (app) => {
   const modules = require('../controllers/module.controller');
-
   app.post('/modules', modules.create);
-  
   app.get('/modules', modules.findAll);
   app.get('/modules/:module_id', modules.findOne);
-
   app.put('/modules/:module_id', modules.update);
-
   app.delete('/modules/:module_id', modules.delete);
-
 };
