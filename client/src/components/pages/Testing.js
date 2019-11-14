4e2766fb9773cb6ab65699c594b927ac5ce5b97a
Livan Jimenez <jimenezlivan14@gmail.com>
2019-11-14 17:46:11 -0500

    remove

--- client/src/components/pages/Testing.js
+++ client/src/components/pages/Testing.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Content } from 'mui-layout';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import Modules from '../modules/Module.DisplayData';
 
 const useStyles = makeStyles(theme => ({
   title: {
